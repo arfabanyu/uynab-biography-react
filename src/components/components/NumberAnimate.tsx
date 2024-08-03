f1349b57a06f194799fe3c8f8dca5d78a19ad803
Banyu_sans <banyuarfa1122@gmail.com>
2024-08-03 05:49:24 -0700

    v1.1.0

--- src/components/components/NumberAnimate.tsx
+++ src/components/components/NumberAnimate.tsx
@@ -1,11 +1,13 @@
 import { animate, useMotionValue, useTransform, motion } from "framer-motion";
 import { useEffect } from "react";
-
-export default function PercentAnimate({ children }) {
+type PercentAnimate = {
+  children: number;
+};
+export default function PercentAnimate({ children }: PercentAnimate) {
   const count = useMotionValue(0);
   const rounded = useTransform(count, (latest) => Math.round(latest));
   useEffect(() => {
-    const controls = animate(count, parseInt(children));
+    const controls = animate(count, children);
 
     return () => controls.stop();
   }, []);
